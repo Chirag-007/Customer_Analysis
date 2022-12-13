826d979943879ef51dd0fac9f87ff7caa51ec5d8
Chirag-007 <chiragavk123@gmail.com>
2022-12-13 18:16:17 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project to analyze customer data and provide information using it',
+    author='Chirag and Chethan',
+    license='MIT',
+)
